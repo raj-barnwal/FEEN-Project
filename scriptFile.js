13bb57448e36af738e48b78c32dbf82703b1e435
raj-barnwal <raj.barnwal@tothenew.com>
2017-03-08 17:39:05 +0530

    Basket Project

---
+++ scriptFile.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+    /*delete row*/
+    $('.glyphicon-trash').on('click', function () {
+        $(this).parents('tr').remove();
+        calculateAmount();
+
+    });
+
+    /*Cost corresponding price and quantity */
+    var cost = 0;
+    $(".qty").on('change', function () {
+        $(".qty").each(function () {
+            var quantity = this.value;
+            var price = parseFloat(this.closest("tr").children[1].innerText);
+            cost = quantity * price;
+            this.closest("tr").children[3].innerText = cost;
+            calculateAmount();
+        });
+
+    });
+
+    function calculateAmount() {
+        /*Subtotal*/
+        var sum = 0;
+        $('.addAmount').each(function () {
+            sum += parseFloat($(this).text());
+        });
+        $('#subTotal').html(sum.toFixed(2));
+
+        /*Subtotal amount after vat calculation*/
+        var stotal = 0;
+        $('#subTotal').each(function () {
+            stotal = $(this).text() * 0.2;
+        });
+        $('#vatAmount').html(stotal.toFixed(2));
+
+        /*Total Cost*/
+        var totalcost = 0;
+        $('#subTotal,#vatAmount').each(function () {
+            totalcost += parseFloat($(this).text());
+        });
+        $('#tatalCost').html(totalcost.toFixed(2));
+
+    }
+
+})
+
+
+
+
+
+
+
